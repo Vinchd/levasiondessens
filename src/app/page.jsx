c61cf33c24e5872ab62004fbab1ed1c3a841951c
Vinchd <vince.daviaud@gmail.com>
2025-06-08 17:02:12 +0200

    initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+	return (
+		<div>
+			{/* <div className="justify-items-center items-center gap-16 grid grid-rows-[20px_1fr_20px] p-8 sm:p-20 pb-20 min-h-screen"> */}
+			<main className="flex flex-col items-center sm:items-start gap-[32px] row-start-2">
+				Toto
+			</main>
+		</div>
+	);
+}
