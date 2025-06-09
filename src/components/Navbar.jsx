4d3fd1cb964253dfda4f631ec1745463e86c5de8
Vinchd <vince.daviaud@gmail.com>
2025-06-09 03:19:29 +0200

    Navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,31 +5,41 @@ import { FaFacebookSquare } from "react-icons/fa";
 
 export default function Navbar() {
 	return (
-		<nav className="top-0 fixed bg-primary w-full">
-			<div className="flex items-center mx-auto px-12 py-4">
-				<Link href="/" className="mr-auto">
+		<nav className="top-0 fixed bg-primary py-4 w-full h-32 max-md:h-[90px]">
+			<div className="flex items-center mx-auto px-12 max-md:px-5 h-full">
+				<Link href="/" className="flex items-center mr-auto h-full">
 					<Image
 						src="/evasion-des-sens-logo.png"
 						alt="Logo Evasion des Sens"
-						width={151}
-						height={109}
+						width={101}
+						height={59}
+						className="w-auto h-full object-contain"
 					/>
 				</Link>
-				<Link href="/contact">Contact</Link>
-				<a
-					href="https://www.instagram.com/levasiondes.sens/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<AiFillInstagram className="w-14 h-14" />
-				</a>
-				<a
-					href="https://www.facebook.com/profile.php?id=100067921822699"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<FaFacebookSquare className="w-13 h-13" />
-				</a>
+				<div className="flex items-center gap-5 max-md:gap-2 md:mr-7 h-full">
+					<Link
+						href="/contact"
+						className="flex items-center px-3 max-md:px-1 h-full hover:text-secondary hover:underline underline-offset-3"
+					>
+						Contact
+					</Link>
+					<a
+						href="https://www.instagram.com/levasiondes.sens/"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="flex items-center px-3 max-md:px-1 h-full hover:text-secondary"
+					>
+						<AiFillInstagram className="w-8 h-8" />
+					</a>
+					<a
+						href="https://www.facebook.com/profile.php?id=100067921822699"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="flex items-center px-3 max-md:px-1 h-full hover:text-secondary"
+					>
+						<FaFacebookSquare className="w-7 h-7" />
+					</a>
+				</div>
 			</div>
 		</nav>
 	);
