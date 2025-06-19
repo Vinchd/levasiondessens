e5c6e596929d05e15444236cac0380f55ba9b2ae
Vinchd <vince.daviaud@gmail.com>
2025-06-19 20:49:32 +0200

    add jsonLd for seo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -46,12 +46,53 @@ export const metadata = {
 	},
 };
 
+const jsonLd = {
+	"@context": "https://schema.org",
+	"@type": "BeautySalon",
+	name: "L'Évasion des Sens",
+	image: "https://levasiondessens.vercel.app/evasion-des-sens-logo.png",
+	url: "https://levasiondessens.vercel.app",
+	telephone: "+33636180561",
+	address: {
+		"@type": "PostalAddress",
+		streetAddress:
+			"Le grand large, 17 Rue de la Corderie, 44760 La Bernerie-en-Retz",
+		addressLocality: "La Bernerie-en-Retz",
+		postalCode: "44760",
+		addressCountry: "FR",
+	},
+	openingHoursSpecification: [
+		{
+			"@type": "OpeningHoursSpecification",
+			dayOfWeek: [
+				"Monday",
+				"Tuesday",
+				"Wednesday",
+				"Thursday",
+				"Friday",
+				"Saturday",
+			],
+			opens: "10:00",
+			closes: "19:00",
+		},
+	],
+	sameAs: [
+		"https://www.instagram.com/levasiondes.sens/",
+		"https://www.facebook.com/profile.php?id=100067921822699",
+	],
+};
+
 export default function RootLayout({ children }) {
 	return (
 		<html lang="fr" className="h-full">
 			<body
 				className={`${poppins.className} antialiased flex min-h-full flex-col`}
 			>
+				<script
+					type="application/ld+json"
+					dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+				/>
+
 				<Navbar />
 				<main className="flex justify-center bg-secondary pt-28 max-md:pt-[90px] h-[calc(100vh-8rem)] grow">
 					{children}
