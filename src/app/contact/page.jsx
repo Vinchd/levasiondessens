a41f81e6fb6d7c2c071e549f7ecfe199d1def742
Vinchd <vince.daviaud@gmail.com>
2025-06-19 04:22:44 +0200

    add css transition on hover

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -14,7 +14,7 @@ export default function page() {
 						href="https://www.planity.com/levasion-des-sens-44760-la-bernerie-en-retz"
 						target="_blank"
 						rel="noopener noreferrer"
-						className="font-semibold text-primary hover:text-gray-800 underline"
+						className="font-semibold text-primary hover:text-gray-800 underline transition duration-200"
 					>
 						Planity
 					</a>
@@ -27,7 +27,7 @@ export default function page() {
 						Tel :{" "}
 						<a
 							href="tel:+33636180561"
-							className="text-primary hover:text-[#c5a58c]"
+							className="text-primary hover:text-[#c5a58c] transition duration-200"
 						>
 							06 36 18 05 61
 						</a>
@@ -36,7 +36,7 @@ export default function page() {
 						Email :{" "}
 						<a
 							href="mailto:evasiondessens44@gmail.com"
-							className="text-primary hover:text-[#c5a58c]"
+							className="text-primary hover:text-[#c5a58c] transition duration-200"
 						>
 							evasiondessens44@gmail.com
 						</a>
@@ -45,47 +45,5 @@ export default function page() {
 				<ContactForm />
 			</div>
 		</div>
-		// <div className="flex justify-center bg-secondary max-md:pt-[90px] w-full h-[calc(100vh-8rem)] text-sm grow">
-		// 	<div className="flex flex-col gap-5 my-auto max-md:px-4 w-full max-w-[900px] overflow-y-auto">
-		// 		<div className="text-center tracking-wider">
-		// 			Une question ? Besoin d’un conseil ou d’une information sur nos
-		// 			prestations ? Laissez-nous un message, nous vous répondrons avec
-		// 			plaisir dans les plus brefs délais. <br />
-		// 			Vous pouvez rapidement et simplement prendre rendez-vous sur&nbsp;
-		// 			<a
-		// 				href="https://www.planity.com/levasion-des-sens-44760-la-bernerie-en-retz"
-		// 				target="_blank"
-		// 				rel="noopener noreferrer"
-		// 				className="font-semibold text-primary hover:text-gray-800 underline"
-		// 			>
-		// 				Planity
-		// 			</a>
-		// 		</div>
-		// 		<div className="flex justify-center h-10">
-		// 			<BookButton text="Réserver en ligne" />
-		// 		</div>
-		// 		<div className="flex flex-col items-center">
-		// 			<p>
-		// 				Tel :{" "}
-		// 				<a
-		// 					href="tel:+33636180561"
-		// 					className="text-primary hover:text-[#c5a58c]"
-		// 				>
-		// 					06 36 18 05 61
-		// 				</a>
-		// 			</p>
-		// 			<p>
-		// 				Email :{" "}
-		// 				<a
-		// 					href="mailto:evasiondessens44@gmail.com"
-		// 					className="text-primary hover:text-[#c5a58c]"
-		// 				>
-		// 					evasiondessens44@gmail.com
-		// 				</a>
-		// 			</p>
-		// 		</div>
-		// 		<ContactForm />
-		// 	</div>
-		// </div>
 	);
 }
